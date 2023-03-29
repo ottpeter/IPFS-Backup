4c426e3fe297fc3078007b10dae7633956ad872d
Péter Ott <ottpeter@tuta.io>
2023-03-29 13:14:32 -0400

    Copies components from fevm-starter-kit to the root of the repository

---
+++ deploy/00_deploy.js
@@ -0,0 +1,29 @@
+require("hardhat-deploy")
+require("hardhat-deploy-ethers")
+
+const { networkConfig } = require("../helper-hardhat-config")
+
+
+const private_key = network.config.accounts[0]
+const wallet = new ethers.Wallet(private_key, ethers.provider)
+
+module.exports = async ({ deployments }) => {
+    console.log("Wallet Ethereum Address:", wallet.address)
+    const chainId = network.config.chainId
+    const tokensToBeMinted = networkConfig[chainId]["tokensToBeMinted"]
+
+
+    //deploy FilecoinMarketConsumer
+    const FilecoinMarketConsumer = await ethers.getContractFactory('FilecoinMarketConsumer', wallet);
+    console.log('Deploying FilecoinMarketConsumer...');
+    const filecoinMarketConsumer = await FilecoinMarketConsumer.deploy();
+    await filecoinMarketConsumer.deployed()
+    console.log('FilecoinMarketConsumer deployed to:', filecoinMarketConsumer.address);
+    
+    //deploy DealClient
+    const DealClient = await ethers.getContractFactory('DealClient', wallet);
+    console.log('Deploying DealClient...');
+    const dc = await DealClient.deploy();
+    await dc.deployed()
+    console.log('DealClient deployed to:', dc.address);
+}
---
+++ tasks/index.js
@@ -0,0 +1,4 @@
+exports.getAddress = require("./get-address")
+exports.storeAll = require("./filecoin-market-consumer/store-all")
+exports.makeDealProposal = require("./deal-client/make-deal-proposal")
+exports.getDealProposal = require("./deal-client/get-deal-proposal")
