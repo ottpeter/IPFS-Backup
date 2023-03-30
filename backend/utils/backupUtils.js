3c0988972dd939beaab81f0dc156d0518924f9b0
Péter Ott <ottpeter@tuta.io>
2023-03-30 15:39:18 -0400

    break

--- backend/utils/backupUtils.js
+++ backend/utils/backupUtils.js
@@ -251,7 +251,7 @@ async function checkDealStatus(folderName) {
       const result = await dealClient.getDealId(commPasBytes);                                        // Send transaction
       dealID = result.toNumber();
       console.log("Deal ID: ", dealID);
-  
+      if (dealID !== 0) break;
       try_count++;
       await delay(1000*60*2);
     } while (try_count < max_try && dealID === 0);
