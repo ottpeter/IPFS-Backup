a622defb5cefb3f21375d8d3124328ef0232f210
Péter Ott <ottpeter@tuta.io>
2023-04-05 10:27:12 -0400

    get-name-array

--- tasks/deal-client/get-name-array.js
+++ tasks/deal-client/get-name-array.js
@@ -1,22 +1,21 @@
 const CID = require('cids');
-// TODO
+
 task(
-    "get-commp",
-    "Get CommP from UniqID"
+    "get-name-array",
+    "Get array of {name, commP} from start value and how-many value"
   )
     .addParam("contract", "The address of the deal client solidity")
-    .addParam("uniqId", "The PieceCID, alias commP")
+    .addParam("from", "Start index of nameLookupArray")
+    .addParam("count", "How many elements we want")
     .setAction(async (taskArgs) => {
         const contractAddr = taskArgs.contract;
         const networkId = network.name;
-        console.log("Getting CommP from UniqID on network", networkId);
+        console.log("Getting nameLookupArray segment on network ", networkId);
 
         const wallet = new ethers.Wallet(network.config.accounts[0], ethers.provider);
         const DealClient = await ethers.getContractFactory("DealClient", wallet);
         const dealClient = await DealClient.attach(contractAddr);
           
-        //send a transaction to call makeDealProposal() method
-        //transaction = await dealClient.getDealProposal(proposalID)
-        let result = await dealClient.getCommpFromId(taskArgs.uniqId);
-        console.log("CommP:", result);
+        let result = await dealClient.getNameLookupArraySegment(taskArgs.from, taskArgs.count);
+        console.log("nameLookupArray segment: ", result);
     })
