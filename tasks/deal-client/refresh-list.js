66244398c6393c534b92d3926d1ba273341d166e
Péter Ott <ottpeter@tuta.io>
2023-04-05 10:44:07 -0400

    refresh-all

---
+++ tasks/deal-client/refresh-list.js
@@ -0,0 +1,22 @@
+const CID = require('cids');
+
+task(
+  "refresh-single",
+  "Refresh metadata for a single backup item"
+)
+  .addParam("contract", "The address of the deal client solidity")
+  .addParam("commPList", "List of backups to be refreshed")
+  .setAction(async (taskArgs) => {
+      const contractAddr = taskArgs.contract;
+      const stringList = taskArgs.commP;
+      const commPasBytesList = stringList.map((commP) => new CID(commP).bytes);
+      const networkId = network.name;
+      console.log("Refreshing backup item on network ", networkId)
+
+      const wallet = new ethers.Wallet(network.config.accounts[0], ethers.provider);        // Create a new wallet instance
+      const DealClient = await ethers.getContractFactory("DealClient", wallet);             // Create a DealClient contract factory
+      const dealClient = await DealClient.attach(contractAddr);                             // Create a contract instance
+        
+      let result = await dealClient.refreshMetadataForList(commPasBytesList);         // Send transaction
+      if (result) console.log(`The backup items for the given list were refreshed.`);
+  })
