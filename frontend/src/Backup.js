bf58dc780a22baa22c8da6d311a2879069be3c2a
Péter Ott <ottpeter@tuta.io>
2023-04-07 06:08:37 -0400

    good

--- frontend/src/Backup.js
+++ frontend/src/Backup.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import { ethers } from 'ethers';
-import axios from 'axios';
 import CID from 'cids';
 import contractObj from "./DealClient.json"
 import { network } from './network';
@@ -20,16 +19,10 @@ export default function Backup() {
     //const dealClient = new ethers.Contract(contractAddr, contractObj.abi, provider);                  // Contract Instance
     //const result = await dealClient.getBackupItem(commPasBytes);                                      // Smart contract call (view)
 
-    /*axios
-      .get(START_URL)
-      .then(function (response) {
-        console.log(response);
-      });*/
-
     const response = await fetch(START_URL, {
       method: "GET",
     });
-    //console.log(await response.json());
+    console.log(await response.json());
   }
 
   return (
