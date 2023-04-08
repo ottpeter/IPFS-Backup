c2237326c1b282f0a2459472678060dfe38856df
Péter Ott <ottpeter@tuta.io>
2023-04-08 15:21:53 -0400

    maxDepth

--- backend/routes/ipfsRoutes.js
+++ backend/routes/ipfsRoutes.js
@@ -5,11 +5,14 @@ const { getFileSystem, createIPFSinstance } = require("../utils/ipfsUtils");
 
 
 router.get('/mfs-tree', async function(req, res) {
-  const maxDepth = req.query.depth || 50;
+  let maxDepth = 50;
+  if (req.query.depth) maxDepth = parseInt(req.query.depth);
+  
   const {ipfs, CID, globSource } = await createIPFSinstance();
   const tree = await getFileSystem(ipfs, '/', 0, maxDepth);
-
+  
   console.log("Tree: ", tree)
+  console.log(maxDepth)
 
   res.json({result: "OK", mfsTree: tree})
 });
